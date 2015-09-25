9c4bc7f8ad2b69ff2bc0a2d2dc519b4244cfd5ba
Sergio Vallejo <vallejosergio@gmail.com>
2015-09-25 16:36:51 -0300

    New method setNamespace

--- ElephantIo.php
+++ ElephantIo.php
@@ -1,44 +1,51 @@
-<?php
-namespace sammaye\elephantio;
-
-use yii\base\Component;
-use ElephantIO\Client;
-use ElephantIO\Engine\SocketIO\Version1X;
-
-class ElephantIo extends Component
-{
-	public $host = 'http://localhost:3000';
+	<?php
+	namespace sammaye\elephantio;
 	
-	public $options = [];
+	use yii\base\Component;
+	use ElephantIO\Client;
+	use ElephantIO\Engine\SocketIO\Version1X;
 	
-	private $_client;
-	
-	
-	public function init()
+	class ElephantIo extends Component
 	{
-		parent::init();
+		public $host = 'http://localhost:3000';
 		
-		$host = $this->host;
+		public $options = [];
 		
-		$this->_client = new Client(new Version1X(is_callable($host) ? $host() : $host, $this->options));
-		$this->_client->initialize();
+		public $namespace = '/';
+		
+		private $_client;
+		
+		
+		public function init()
+		{
+			parent::init();
+			
+			$host = $this->host;
+			
+			$this->_client = new Client(new Version1X(is_callable($host) ? $host() : $host, $this->options));
+			$this->_client->initialize();
+		}
+		
+		public function emit($event, $params = [])
+		{
+			return $this->_client->emit($event, $params);
+		}
+		
+		public function read()
+		{
+			return $this->_client->read();
+		}
+		
+		public function close()
+		{
+			return $this->_client->close();
+		}
+		
+		public function setNamespace($namespace)
+		{
+			if(method_exists ($this->_client,'of'))
+			{
+				$this->_client->of($namespace);  
+			}
+		}
 	}
-	
-	public function emit($event, $params = [], $namespace = '/')
-	{
-		if(method_exists ($this->_client,'of'))    
-                        $this->_client->of($namespace);  
-                        
-		return $this->_client->emit($event, $params);
-	}
-	
-	public function read()
-	{
-		return $this->_client->read();
-	}
-	
-	public function close()
-	{
-		return $this->_client->close();
-	}
-}
